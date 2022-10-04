087ef613c999ee04404769dd2befae16a7ff9c8d
jpalmda <106854574+jpalmda@users.noreply.github.com>
2022-10-04 12:41:36 -0300

    terceiro commit

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = 'Caian.10';
+$dbName = 'cadastro-caian';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+//if($conexao->connect_errno)
+//{
+  //  echo "Erro";
+//}
+//else
+//{
+ //echo "Conxão efetuada com sucesso";
+//}
+?>
---
+++ login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./stylecadastro.css">
+    <title>login</title>
+</head>
+
+<body>
+    <main>
+        <h1>Login</h1>
+        <div class="social-media">
+            <a
+                href="https://accounts.google.com/signin/v2/identifier?service=mail&passive=1209600&osid=1&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin">
+                <img src="./Img/google.png" alt="Google">
+            </a>
+            <a href="https://pt-br.facebook.com">
+                <img src="./Img/facebook.png" alt="Facebook">
+            </a>
+        </div>
+
+        <div class="alternative">
+            <span>OR</span>
+        </div>
+
+        <form action="testLogin.php" method="post">
+            <label for="email">
+                <span>E-mail</span>
+                <input type="email" id="email" name="email">
+            </label>
+
+            <label for="password">
+                <span>Password</span>
+                <input type="password" id="password" name="password">
+            </label>
+            <input type="submit" value="Sign Up" name="button">
+            <p>Se não possui uma conta efetue seu <a href="cadastro.php">Cadastro</a>
+        </form>
+    </main>
+    <section class="images">
+        <img src="./Img/Sangue.png" alt="Sangue">
+    </section>
+</body>
+
+</html>
